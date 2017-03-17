8bd65da35ec4185e29a8081e31a1624a8c8d98e3
Daniel <daniel65536@gmail.com>
2017-03-18 03:31:23 +0800

    fix bugs when feed without pubdate

--- bot.py
+++ bot.py
@@ -8,8 +8,9 @@ import requests
 from lxml import etree, html
 from lxml.html import clean
 from dateutil.parser import parse
-from multiprocessing.dummy import Pool
+from urllib.parse import urljoin
 from pathlib import Path
+from multiprocessing.dummy import Pool
 
 class FeedBot(object):
     def __init__(self):
@@ -33,13 +34,14 @@ class FeedBot(object):
             u'\U0001F1E0-\U0001F1FF'
             ']+', flags=re.UNICODE)
 
-        self.hash = lambda x: xxhash.xxh64(x, seed=0x03).hexdigest()
+        self.mask = 'https://images{}-focus-opensocial.googleusercontent.com/gadgets/proxy?url={}&container=focus'
+        self.hash = lambda x: xxhash.xxh64(x, seed=0x04).hexdigest()
         self.session = requests.Session()
         self.cleaner = clean.Cleaner(**self.configs)
 
     def text(self, r):
-        encoding = r.headers.get('charset')
-        if not encoding:
+        encoding = r.encoding
+        if encoding in [None, 'ISO-8859-1']:
             encodings = requests.utils.get_encodings_from_content(r.text)
             if encodings:
                 encoding = encodings[0]
@@ -48,13 +50,13 @@ class FeedBot(object):
         return self.pattern.sub('', r.content.decode(encoding))
 
     def fetch(self, url, tries=1):
-        if tries > 10:
+        if tries > 3:
             raise Exception
         else:
             print(url)
         try:
-            r = self.session.get(url, headers=self.headers, timeout=tries*10)
-            return self.text(r)
+            r = self.session.get(url, headers=self.headers, timeout=tries*20)
+            return self.text(r).encode('utf-8')
         except:
             return self.fetch(url, tries=tries+1)
 
@@ -63,17 +65,22 @@ class FeedBot(object):
         if cache.exists():
             data = cache.open(encoding='utf-8').read()
         else:
-            page = html.fromstring(self.fetch(url))
+            parser = html.HTMLParser(encoding='utf-8', remove_comments=True, remove_blank_text=True)
+            page = html.fromstring(self.fetch(url), parser=parser)
+
+            for idx, (elem, attr, link, pos) in enumerate(page.iterlinks()):
+                if elem.tag == 'img':
+                    elem.set('src', self.mask.format(idx % 2, urljoin(url, link)))
+
             node = map(self.cleaner.clean_html, page.xpath(xpath))
-            data = map(lambda x: html.tostring(x, encoding='unicode'), node)
-            data = re.sub(r'<(\w+)>\s*</\1>\n*', r'', '\n'.join(data))
+            data = ''.join(map(lambda x: html.tostring(x, encoding='unicode'), node))
             cache.open('w+', encoding='utf-8').write(data)
         return etree.CDATA(data), self.hash(data)
 
     def process(self, config):
         name, url, xpath = config
-        data = self.fetch(url)
-        feed = etree.fromstring(data.encode('utf-8'), etree.XMLParser(recover=True))
+        parser = etree.XMLParser(encoding='utf-8', recover=True, remove_comments=True, remove_blank_text=True)
+        feed = etree.fromstring(self.fetch(url), parser=parser)
         ns = {
             'atom': 'http://www.w3.org/2005/Atom',
             'content': 'http://purl.org/rss/1.0/modules/content/',
@@ -89,13 +96,15 @@ class FeedBot(object):
         items = feed.xpath('//item|//atom:entry', namespaces=ns)
 
         for idx, node in enumerate(items):
-            date = node.xpath('(pubDate|atom:published)/text()', namespaces=ns)[0]
-            if idx >= 10 and time.time() - parse(date).timestamp() >= 86400:
+            pubdate = node.xpath('(pubDate|atom:published)/text()', namespaces=ns)
+            timeout = time.time() - parse(pubdate[0]).timestamp() >= 86400 if pubdate else True
+            if idx >= 10 and timeout:
                 node.getparent().remove(node)
                 continue
             for i in node.xpath('guid|description|content:encoded|atom:id|atom:summary|atom:content', namespaces=ns):
                 node.remove(i)
             link = node.xpath('link/text()|atom:link[@rel="alternate"]/@href', namespaces=ns)[0]
+            link = urljoin(url, link)
             content(node).text, guid(node).text = self.extract(link, xpath)
 
         data = etree.tostring(feed, encoding='unicode')
@@ -107,7 +116,7 @@ def main():
     feedbot = FeedBot()
     configs = re.findall(YAML, open('config.yaml', encoding='utf-8').read(), re.MULTILINE)
 
-    pool = Pool(4)
+    pool = Pool(8)
     pool.map(feedbot.process, configs)
     pool.close()
     pool.join()
