23dafd76c5d28783042b11cd27c28b8aad372962
Daniel <daniel65536@gmail.com>
2017-04-09 18:42:25 +0800

    fix images link

--- bot.py
+++ bot.py
@@ -33,7 +33,6 @@ class FeedBot(object):
             u'\U0001F680-\U0001F6FF'
             u'\U0001F1E0-\U0001F1FF'
             ']+', flags=re.UNICODE)
-
         self.mask = 'https://images{}-focus-opensocial.googleusercontent.com/gadgets/proxy?url={}&container=focus'
         self.hash = lambda x: xxhash.xxh64(x, seed=0x04).hexdigest()
         self.session = requests.Session()
@@ -60,58 +59,62 @@ class FeedBot(object):
         except:
             return self.fetch(url, tries=tries+1)
 
-    def extract(self, url, xpath):
+    def extract(self, url, xpath, fiximg):
         cache = Path('.cache/{}'.format(self.hash(url)))
-        if cache.exists():
-            data = cache.open(encoding='utf-8').read()
-        else:
+        data = cache.open(encoding='utf-8').read() if cache.exists() else None
+
+        if not data:
             parser = html.HTMLParser(encoding='utf-8', remove_comments=True, remove_blank_text=True)
             page = html.fromstring(self.fetch(url), parser=parser)
+            page.make_links_absolute(url)
 
-            for idx, (elem, attr, link, pos) in enumerate(page.iterlinks()):
-                if elem.tag == 'img':
-                    elem.set('src', self.mask.format(idx % 2, urljoin(url, link)))
+            for idx, node in enumerate(page.xpath('//img')):
+                link = urljoin(url, node.get('data-src') or node.get('src'))
+                link = self.mask.format(idx % 2, link) if fiximg == 'true' else link
+                node.set('src', link)
 
             node = map(self.cleaner.clean_html, page.xpath(xpath))
-            data = ''.join(map(lambda x: html.tostring(x, encoding='unicode'), node))
+            data = ''.join(map(lambda x: html.tostring(x, encoding='unicode'), node)).strip()
             cache.open('w+', encoding='utf-8').write(data)
-        return etree.CDATA(data), self.hash(data)
+
+        return data, self.hash(data)
 
     def process(self, config):
-        name, url, xpath = config
+        name, url, xpath, fiximg = config
         parser = etree.XMLParser(encoding='utf-8', recover=True, remove_comments=True, remove_blank_text=True)
         feed = etree.fromstring(self.fetch(url), parser=parser)
         ns = {
             'atom': 'http://www.w3.org/2005/Atom',
             'content': 'http://purl.org/rss/1.0/modules/content/',
+            'feedburner': 'http://rssnamespace.org/feedburner/ext/1.0',
         }
 
         if feed.tag == 'rss':
             content = lambda node: etree.SubElement(node, 'description')
-            guid = lambda node: etree.SubElement(node, 'guid')
+            guid = lambda node: etree.SubElement(node, 'guid', attrib={'isPermaLink': 'false'})
         else:
             content = lambda node: etree.SubElement(node, 'summary', attrib={'type':'html'})
             guid = lambda node: etree.SubElement(node, 'id')
 
-        items = feed.xpath('//item|//atom:entry', namespaces=ns)
-
-        for idx, node in enumerate(items):
+        for idx, node in enumerate(feed.xpath('//item|//atom:entry', namespaces=ns)):
             pubdate = node.xpath('(pubDate|atom:published)/text()', namespaces=ns)
             timeout = time.time() - parse(pubdate[0]).timestamp() >= 86400 if pubdate else True
-            if idx >= 10 and timeout:
+            link = node.xpath('link/text()|atom:link[@rel="alternate"]/@href', namespaces=ns)
+            if idx >= 10 and timeout or link == []:
                 node.getparent().remove(node)
                 continue
             for i in node.xpath('guid|description|content:encoded|atom:id|atom:summary|atom:content', namespaces=ns):
                 node.remove(i)
-            link = node.xpath('link/text()|atom:link[@rel="alternate"]/@href', namespaces=ns)[0]
-            link = urljoin(url, link)
-            content(node).text, guid(node).text = self.extract(link, xpath)
+            content(node).text, guid(node).text = self.extract(urljoin(url, link[0]), xpath, fiximg)
+
+        for node in feed.xpath('//*[@rel="hub"]|//item[./description/text()=""]|//atom:entry[./summary/text()=""]', namespaces=ns):
+            node.getparent().remove(node)
 
         data = etree.tostring(feed, encoding='unicode')
         open('{}.xml'.format(name), 'w+', encoding='utf-8').write(data)
 
 def main():
-    YAML = r'^(.*?):\s*$\s*url:\s*(.*?)\s*$\s*xpath:\s*(.*?)\s*$'
+    YAML = r'^(.*?):\s*$\s*url:\s*(.*?)\s*$\s*xpath:\s*(.*?)\s*$\s*fiximg:\s*(.*?)\s*$'
 
     feedbot = FeedBot()
     configs = re.findall(YAML, open('config.yaml', encoding='utf-8').read(), re.MULTILINE)
